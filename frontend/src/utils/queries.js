b8a5dd163ec1de78ad72a9480a01741876d9a050
coleleg <coleleg@gmail.com>
2022-03-29 20:17:49 -0400

    change employee query

--- frontend/src/utils/queries.js
+++ frontend/src/utils/queries.js
@@ -18,6 +18,7 @@ export const QUERY_GYM = gql`
 export const QUERY_MEMBERS = gql`
 query Query {
     gymMembers {
+        _id
         members {
             _id
             firstName
@@ -32,15 +33,16 @@ query Query {
 `;
 
 export const QUERY_EMPLOYEES = gql`
-    query Query {
-        gymEmployees {
+query GymEmployees {
+    gymEmployees {
+        _id
             employees {
-                _id
                 firstName
                 lastName
                 email
                 phoneNumber
                 admin
+                _id
             }
         }
     }
